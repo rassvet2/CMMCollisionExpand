19dafa91a2337a9c2c92065b583fac418d00e08e
rassvet_ii <dekabrist19480705@gmail.com>
2025-06-06 03:37:21 +0900

    Replace placeholders

---
+++ common/src/main/java/com/rassvet_ii/cmmce/Constants.java
@@ -0,0 +1,11 @@
+package com.rassvet_ii.cmmce;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "cmmce";
+	public static final String MOD_NAME = "CMMCollisionExpand";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ forge/src/main/java/com/rassvet_ii/cmmce/platform/ForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package com.rassvet_ii.cmmce.platform;
+
+import com.rassvet_ii.cmmce.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
