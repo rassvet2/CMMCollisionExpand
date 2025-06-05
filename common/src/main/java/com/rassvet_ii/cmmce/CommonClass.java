a0e3f71f69d81abb046bf6a856de39e419c23f5e
rassvet_ii <dekabrist19480705@gmail.com>
2025-06-06 04:30:27 +0900

    Migrate to architectury

--- common/src/main/java/com/rassvet_ii/cmmce/CommonClass.java
+++ common/src/main/java/com/rassvet_ii/cmmce/CommonClass.java
@@ -1,8 +1,8 @@
 package com.rassvet_ii.cmmce;
 
 import com.rassvet_ii.cmmce.platform.Services;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Items;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
 
 // This class is part of the common project meaning it is shared between all supported loaders. Code written here can only
 // import and access the vanilla codebase, libraries used by vanilla, and optionally third party libraries that provide
@@ -16,7 +16,7 @@ public class CommonClass {
     public static void init() {
 
         Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
-        Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
+        Constants.LOG.info("The ID for diamonds is {}", Registries.ITEM.getKey(Items.DIAMOND));
 
         // It is common for all supported loaders to provide a similar feature that can not be used directly in the
         // common code. A popular way to get around this is using Java's built-in service loader feature to create
